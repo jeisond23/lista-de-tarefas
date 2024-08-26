bd0ea83644dc77c1ae8599ff26ce8b22ef3cb804
Jeison SR <jeison.sr.cursos@gmail.com>
2024-08-26 19:58:25 -0300

    Primeira parte do projeto finalizada.

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,26 @@
 <script>
-// Código Javascript aqui
+  // Código Javascript aqui
+  import Header from './components/Header.vue';
+  import Conteudo from './components/Conteudo.vue';
+  import Footer from './components/Footer.vue';
   export default {
+    components: {
+      Header,
+      Conteudo,
+      Footer
+    },
     data() {
         return {
-            message: "Bem Vindo Fulano!!!",
-            corpo: "Corpo do texto",
-            count: 0
         }
     }
   }
 </script>
 
 <template>
-  <header class="mb-5" id="header">
-      <!-- Cabeçalho -->
-      <nav class="navbar navbar-light border-bottom">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-              <img src="./assets/img/logo-firjan.png" alt="Logo" class="logo-firjan">
-            </a>
-          </div>
-      </nav>
-      <div class="container-fluid mt-5 text-center">
-          <h1>Curso Programador FrontEnd</h1>
-          <!-- <h2>Disciplina: Desenvolvimento Web</h2>
-          <h3>{{ message }}</h3>
-          <h4>{{ corpo }}</h4> -->
-      </div>
-  </header>
-  <main class="container" id="app">
-      <!-- Conteúdo -->
-      <h3>{{ message }}</h3>
-      <h4>{{ corpo }}</h4>
-      <hr>
-
-      <p v-show="count > 0">Contagem atual: {{ count }}</p>
-
-      <button class="btn btn-warning" @click="count--" :disabled="count <= 0">Decrementar</button> | 
-      <button class="btn btn-primary" @click="count++">Incrementar</button>
-
-      <div v-if="count > 0">
-          <p>Objeto count está sendo atualizado.</p>
-      </div>
-      <div v-else>
-          <p>Objeto count tem como valor o 0.</p>
-      </div>
-
-  </main>
-  <footer class="container-fluid fixed-bottom bg-dark">
-      <!-- Rodapé -->
-        <div class="f-info">
-          <span>Desenvolvido por [SEU NOME] 2024&COPY;</span>
-        </div>
-  </footer>
+  <!-- Blocos/Componentes aqui -->
+  <Header />
+  <Conteudo />
+  <Footer />
 </template>
 
 <style scoped>
---
+++ src/components/Conteudo.vue
@@ -0,0 +1,79 @@
+<script>
+    // Código Javascript aqui
+    export default {
+        data() {
+            return {
+                // Propriedades aqui.
+            }
+        },
+    }
+</script>
+<template>
+    <!-- Código HTML aqui -->
+    <h2 class="subtitulo">Lista de tarefas</h2>
+    <div class="d-flex flex-column align-items-center container-fluid">
+        <p class="card item text-bg-secondary">
+                Tarefa 1
+        </p>
+        <p class="card item text-bg-secondary">
+                Tarefa 2
+        </p>
+        <p class="card item text-bg-secondary">
+                Tarefa 3
+        </p>
+    </div>
+    <div class="d-flex flex-column container-fluid">
+        <form @submit.prevent class="d-flex flex-column align-items-center container">
+            <input
+                type="text"
+                placeholder="Adicione uma tarefa..."
+                class="form-control"
+            />
+            <span class="text-danger m-3">MENSAGEM DE ERRO!!!</span>
+            <div class="d-flex flex-row justify-content-around">
+                <button class="btn btn-primary">
+                    Adicionar
+                </button>
+                <button class="btn btn-danger">
+                    Limpar lista
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
+<style scope>
+    /* Estilização CSS aqui */
+    .d-flex button {
+        width: 160px;
+        height: 50px;
+        margin: 15px;
+        padding: 5px;
+    }
+
+    .subtitulo {
+        font-size: 35px;
+        margin-bottom: 25px;
+        padding-bottom: 5px;
+        text-decoration: underline;
+        text-align: center;
+        font-weight: bold;
+    }
+
+    .item {
+        font-size: 30px;
+        font-weight: bold;
+        text-align: center;
+        line-height: 25px;
+        width: 60%;
+        padding: 25px;
+    }
+
+    .item:hover {
+        cursor: pointer;
+    }
+
+    .concluida {
+        text-decoration: line-through;
+    }
+
+</style>
