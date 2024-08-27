79e4d2ad83a9b371ad3427d49ff48d56ae630eec
Jeison SR <jeison.sr.cursos@gmail.com>
2024-08-27 19:33:48 -0300

    Finalizacao da lista de tarefas.

--- src/components/Conteudo.vue
+++ src/components/Conteudo.vue
@@ -4,37 +4,67 @@
         data() {
             return {
                 // Propriedades aqui.
+                tarefas: [],
+                novaTarefa: {
+                    texto: "",
+                    concluida: false
+                },
+                error: ""
             }
         },
+        methods: {
+            adicionarTarefa: function(){
+                // Código aqui
+                if(this.novaTarefa.texto) {
+                    this.tarefas.push(this.novaTarefa);
+                    this.novaTarefa = {
+                        concluida: false
+                    };
+                    this.error = "";
+                    localStorage.setItem("tarefas", JSON.stringify(this.tarefas));
+                } else {
+                    this.error = "Você não informou a tarefa!!!";
+                    // alert("Uma tarefa deve ser informada!!!");
+                }
+            },
+            removerTarefas: function() {
+                // Código aqui
+                localStorage.removeItem("tarefas");
+                this.tarefas = [];
+                if(this.error) this.error = "";
+                alert("Lista de tarefas limpa!!!");
+            }
+        },
+        created() {
+            this.tarefas = localStorage.getItem("tarefas") ? JSON.parse(localStorage.getItem("tarefas")) : this.tarefas;
+        },
+        updated() {
+            localStorage.setItem("tarefas", JSON.stringify(this.tarefas));
+        }
     }
 </script>
 <template>
     <!-- Código HTML aqui -->
     <h2 class="subtitulo">Lista de tarefas</h2>
     <div class="d-flex flex-column align-items-center container-fluid">
-        <p class="card item text-bg-secondary">
-                Tarefa 1
-        </p>
-        <p class="card item text-bg-secondary">
-                Tarefa 2
-        </p>
-        <p class="card item text-bg-secondary">
-                Tarefa 3
+        <p class="card item text-bg-secondary" v-for="tarefa in tarefas">
+                {{ tarefa.texto }}
         </p>
     </div>
-    <div class="d-flex flex-column container-fluid">
+    <div class="d-flex flex-column container-fluid margen-b">
         <form @submit.prevent class="d-flex flex-column align-items-center container">
             <input
                 type="text"
                 placeholder="Adicione uma tarefa..."
                 class="form-control"
+                v-model="novaTarefa.texto"
             />
-            <span class="text-danger m-3">MENSAGEM DE ERRO!!!</span>
+            <span class="text-danger m-3" v-show="error">{{ error }}</span>
             <div class="d-flex flex-row justify-content-around">
-                <button class="btn btn-primary">
+                <button class="btn btn-primary" @click="adicionarTarefa()">
                     Adicionar
                 </button>
-                <button class="btn btn-danger">
+                <button class="btn btn-danger" @click="removerTarefas()">
                     Limpar lista
                 </button>
             </div>
@@ -74,6 +104,9 @@
 
     .concluida {
         text-decoration: line-through;
+    }
+    .margen-b {
+        margin-bottom: 80px;
     }
 
 </style>
